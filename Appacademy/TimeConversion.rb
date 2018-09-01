685165bfbca576a24d2ba83c5d63be28674f4c02
ibrahimelbanna <proghima54@gmail.com>
2018-09-01 16:46:46 +0300

    Solving more problems from Appacadmy

---
+++ Appacademy/TimeConversion.rb
@@ -0,0 +1,24 @@
+# Write a method that will take in a number of minutes, and returns a
+# string that formats the number into `hours:minutes`.
+#
+# Difficulty: easy.
+
+def time_conversion(minutes)
+ hours = minutes/60 
+ rest_of_minutes = minutes % 60 
+ if rest_of_minutes < 10 
+   rest_of_minutes = "0"+rest_of_minutes.to_s   
+ end
+return hours.to_s + ":" + rest_of_minutes.to_s
+
+end
+
+# These are tests to check that your code is working. After writing
+# your solution, they should all print true.
+
+puts("\nTests for #time_conversion")
+puts("===============================================")
+    puts('time_conversion(15) == "0:15": ' + (time_conversion(15) == '0:15').to_s)
+    puts('time_conversion(150) == "2:30": ' + (time_conversion(150) == '2:30').to_s)
+    puts('time_conversion(360) == "6:00": ' + (time_conversion(360) == '6:00').to_s)
+puts("===============================================")
